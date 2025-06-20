e8ecfa51433329e2fd4ee0b361c19925f885c683
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-06-21 00:23:22 +0530

    added routes to respective pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/auth/login";
+import Signup from "./pages/auth/signup";
+import Home from "./pages/dashboard/home";
+import Income from "./pages/dashboard/income";
+import Expense from "./pages/dashboard/expense";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/dashboard" element={<Home />}></Route>
+          <Route path="/income" element={<Income />}></Route>
+          <Route path="/expense" element={<Expense />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+const Root = () => {
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  return isAuthenticated ? (
+    <Navigate to="/dashboard"></Navigate>
+  ) : (
+    <Navigate to="/login"></Navigate>
+  );
+};
