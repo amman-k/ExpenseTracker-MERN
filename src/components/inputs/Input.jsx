8176522b2b73f723087de6f49b973168ccef966a
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-06-24 01:22:35 +0530

    added component for login page text boxes

---
+++ src/components/inputs/Input.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+
+const Input = (props) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div>
+      <label className="text-[13px] text-slate-800">{props.label}</label>
+
+      <div className="input-box">
+        <input
+          type={showPassword ? "text" : "password"}
+          placeholder={props.placeholder}
+          className="w-full bg-transparent outline-none"
+          value={props.value}
+          onChange={(e) => {
+            props.onChange(e);
+          }}
+        />
+        <div
+          className="transform cursor-pointer"
+          onClick={toggleShowPassword}
+        >
+          {props.type === 'password' && (showPassword ? (
+            <FaRegEyeSlash size={22} className="primary-color"/>
+          ) : (
+            <FaRegEye size={22} className="primary-color" />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Input;
