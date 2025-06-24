01fa4b524495f4fab047cba9bd14a7e294d3c5df
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-06-25 01:24:20 +0530

    minor changes to styling

--- src/pages/auth/signup.jsx
+++ src/pages/auth/signup.jsx
@@ -37,7 +37,7 @@ const Signup = () => {
 
   return (
     <AuthLayout>
-      <div className="lg:w-[100%] h-auto md: h-full mt-10 md:mt-0 flex flex-col justify-center">
+      <div className="lg:w-[100%] h-auto md: mt-10 md:mt-0 flex flex-col justify-center">
         <h3 className="text-xl font-semibold text-black">Create an Account</h3>
         <p className="text-xs text-slate-700 mt-[5px] mb-6">
           Please enter your details below.
