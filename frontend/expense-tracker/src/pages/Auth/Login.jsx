ded64ba72f64653c574fa3ea16fb43404b3ad1c5
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-07-03 17:26:36 +0530

    created login page

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Authlayout from '../../components/layouts/Authlayout'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <Authlayout>
+      <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center'>
+        <h3 className='text-xl font-semibold text-black'>Welcome Back</h3>
+        <p className='text-xs text-slate-700 mt-[5px] mb-6'>Please enter your details to login</p>
+      </div>
+    </Authlayout>
   )
 }
 
