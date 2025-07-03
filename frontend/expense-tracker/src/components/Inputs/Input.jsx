dc1a9f2d0da66e181525e191b364ca6d60a51cc1
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-07-04 01:12:36 +0530

    fixed the eye icon issue

--- frontend/expense-tracker/src/components/Inputs/Input.jsx
+++ frontend/expense-tracker/src/components/Inputs/Input.jsx
@@ -1,36 +1,40 @@
 import React from "react";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
 
-const Input = ({ value, onChange, placeholder, type, label }) => {
-  const [showPassword, setShowPassword] = React.useState(false);
-  const toggleShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
+const Input = ({
+  value,
+  onChange,
+  placeholder,
+  type,
+  label,
+  showPassword,
+  toggleShowPassword, // We'll use this prop to determine if it's a password field
+}) => {
   return (
-    <div>
-      <label className="text-[13px] text-slate-800 ">{label}</label>
-      <div className="input-box">
+    <div className="relative">
+      <label className="text-[13px] text-slate-800">{label}</label>
+      <div className="input-box relative">
         <input
-          type={
-            type === "password"  && showPassword ? "text" : "password"
-          }
+          type={type}
           placeholder={placeholder}
           className="w-full bg-transparent outline-none"
           value={value}
-          onChange={(e) => onChange(e)}
-        ></input>
+          onChange={onChange}
+        />
 
-        {type === "password" && 
-          
-            (showPassword?(
-            <FaRegEye
-              size={22}
-              className="text-primary cursor-pointer "
-              onClick={() => toggleShowPassword()}
-            />
-            ):(<FaRegEyeSlash size={22} className="text-slate-400 cursor-pointer" onClick={()=>toggleShowPassword}/>))
-          
-        }
+        {/* Render the eye icon only if toggleShowPassword function is provided */}
+        {toggleShowPassword && (
+          <div
+            className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+            onClick={toggleShowPassword}
+          >
+            {showPassword ? (
+              <FaRegEye size={22} className="text-primary" />
+            ) : (
+              <FaRegEyeSlash size={22} className="text-slate-400" />
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
