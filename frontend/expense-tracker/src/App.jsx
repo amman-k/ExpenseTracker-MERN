7c384a46a77ff64c0754912e7f855889133671e4
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-07-03 17:17:56 +0530

    added routes

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/Auth/Login.jsx";
+import Signup from "./pages/Auth/Signup.jsx";
+import Home from "./pages/Dashboard/Home.jsx";
+import Income from "./pages/Dashboard/Income.jsx";
+import Expense from "./pages/Dashboard/Expense.jsx";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signUp" exact element={<Signup />} />
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="/income" exact element={<Income />} />
+          <Route path="/expense" exact element={<Expense />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
+
+const Root = () => {
+  const isAuthenticated = !!localStorage.getItem("token");
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
